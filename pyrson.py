5479980eaa9ddfa55d39d5ecebb7eb770c919687
Andreas Röckert <andreas.rockert@gmail.com>
2022-02-11 23:50:22 +0100

    Add logging of incorrect personal numbers

--- pyrson.py
+++ pyrson.py
@@ -1,22 +1,26 @@
 '''
 A validator for personal numbers.
 '''
+import logging
+from typing import Optional
 
 
 def not_empty(personnr: str):
+    # assert equivant to if <contition>: raise AssertionError
     assert len(personnr) > 0, f"Personal number is empty: {personnr}"
 
 
-def not_null(personnr: str):
-    assert personnr != None, f"Personal number is Null: {personnr}"
+def not_null(personnr: Optional[str] = None):
+    assert personnr is not None, f"Personal number is Null: {personnr}"
 
 
 def correct_len(personnr: str):
-    assert len(
-        personnr) == 10, f"Persnoal number is not of correct length: {personnr} of len: {len(personnr)}"
+    assert len(personnr) == 10, \
+        f"Persnoal number is not of correct \
+        length: {personnr} of len: {len(personnr)}"
 
 
-def is_personal_number(personnr: str):
+def checksum(personnr: str):
 
     string_list = list(personnr)
     integer_list = list(map(int, string_list))
@@ -37,34 +41,54 @@ def is_personal_number(personnr: str):
     accumulator = 10 - accumulator
     accumulator = accumulator % 10
 
-    print(accumulator, last_digit)
-
-    assert accumulator == last_digit, "Not a valid personal number"
+    assert accumulator == last_digit, "Checksum does not match"
 
 
 def remove_non_numerics(personnr: str) -> str:
-    return "".join(filter(str.isdigit, number))
+    return "".join(filter(str.isdigit, personnr))
 
 
 def crop_to_right_size(personnr: str) -> str:
     return personnr[2:] if len(personnr) == 12 else personnr
 
 
-def personal_number_validator(number: str):
+def is_personal_number(personnr: str) -> bool:
     '''
     Personal number validator.
     '''
-    not_null(number)
-    not_empty(number)
 
-    number = remove_non_numerics(number)
-    number = crop_to_right_size(number)
+    # run tests
+    try:
+        not_null(personnr)
+    except AssertionError as err:
+        logger.exception(f"%s unsanitized_input %s", err, personnr)
+        return False
 
-    correct_len(number)
-    is_personal_number(number)
+    # input sanitation
+    sanitized_number = remove_non_numerics(personnr)
+    sanitized_number = crop_to_right_size(sanitized_number)
+
+    # run tests
+    try:
+        not_empty(sanitized_number)
+        correct_len(sanitized_number)
+        checksum(sanitized_number)
+    except AssertionError as err:
+        logger.exception(f"{err} unsanitized_input {personnr}")
+        return False
+    else:
+        return True
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        filename='pyrson.log',
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO
+    )
+    logger = logging.getLogger(__name__)
+    # logger.info("info level")
+
     numbers = [
         "",
         "19780202-2389",
@@ -72,9 +96,9 @@ if __name__ == "__main__":
         "197802022389",
         "7802022389",
         "802022389",
-        # None,
+        None,
         "19820411-2380"
     ]
 
     for number in numbers:
-        personal_number_validator(number)
+        print(number, is_personal_number(number))
