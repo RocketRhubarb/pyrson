efe1e41ecb51aa349c02c3cf92bb59f8d535e289
Andreas Röckert <andreas.rockert@gmail.com>
2022-02-12 22:30:46 +0100

    Update test to more accuretely test each function

--- pyrson_test.py
+++ pyrson_test.py
@@ -3,53 +3,48 @@ Tests for pyrson personal number validator
 '''
 import pytest
 
-from pyrson import is_personal_number
-
-
-def test_sanitation_characters():
-    '''Test input with incorrect characters, expected to return False'''
-    assert is_personal_number('helloworld') is False
-    assert is_personal_number('Robert\'); DROP TABLE Students;--') is False
+from pyrson import *
 
 
 @pytest.mark.parametrize('number', ['19780202-2389', '19820411-2380'])
-def test_checksum_with_known_numbers(number):
+def test_is_personal_number(number):
     '''Test checksum with known working personal numbers'''
     assert is_personal_number(number)
 
 
+def test_sanitation_characters():
+    '''Test input with incorrect characters, expected to return False'''
+    assert remove_non_numerics('helloworld') is ''
+    assert remove_non_numerics('Robert\'); DROP TABLE Students;--') is ''
+
+
 @pytest.mark.parametrize('number', ['19780202-2388', '19820411-2381'])
-def test_checksum_with_known_bad_numbers(number):
-    '''Test known not working personal numbers'''
-    assert is_personal_number(number) is False
+def test_failing_checksum(number):
+    with pytest.raises(Exception):
+        checksum(number)
 
 
 def test_null_input():
     '''Test None input, expected to return False'''
-    assert is_personal_number(None) is False
+    with pytest.raises(Exception):
+        not_null(None)
 
 
 def test_empty_number():
     '''Test Empty input, expected to return False'''
-    assert is_personal_number('') is False
+    with pytest.raises(Exception):
+        not_empty(None)
 
 
 @pytest.mark.parametrize(
     'number',
-    ['1978020-2389', '19780202-238', '24249-23', '14141414141']
+    ['1978020-2389', '19780202-238', '24249-23', '14141414141',
+     '1978020-238419', '1978110202-21438', '1414141412121141']
 )
-def test_too_short_number(number):
+def test_correct_length(number):
     '''Test to short number, expected to return False'''
-    assert is_personal_number(number) is False
-
-
-@pytest.mark.parametrize(
-    'number',
-    ['1978020-238419', '1978110202-21438', '1414141412121141']
-)
-def test_too_long_number(number):
-    '''Test to long number, expected to return False'''
-    assert is_personal_number(number) is False
+    with pytest.raises(Exception):
+        correct_len(number)
 
 
 def test_known_bugs():
@@ -58,3 +53,4 @@ def test_known_bugs():
     but probably should not
     '''
     assert is_personal_number('hello19820411-2380world')
+    assert remove_non_numerics('hello19820411-2380world') == '198204112380'
